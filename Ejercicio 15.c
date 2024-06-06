b2e49d861c2255b43a23128ce7004f83ce28d1b2
nahuelchuca <nahuelchuca2019@gmail.com>
2024-06-06 18:34:55 -0300

    Modificado

--- Ejercicio 15.c
+++ Ejercicio 15.c
@@ -13,8 +13,7 @@ int main(){
     int CantidadDeParticipantes=5, CantidadDeDisparos=10;
     char Participantes[100][5];
     int disparosX, disparosY;
-    int AcumuladorCuadrante1, AcumuladorCuadrante2, AcumuladorCuadrante3, AcumuladorCuadrante4, AcumuladorBlanco;
-    int Puntaje[CantidadDeParticipantes];
+    int AcumuladorCuadrante1, AcumuladorCuadrante2, AcumuladorCuadrante3, AcumuladorCuadrante4, AcumuladorBlanco, AcumuladorEje;
 
     //Inicializar los acumuladores
     AcumuladorBlanco=0;
@@ -22,6 +21,8 @@ int main(){
     AcumuladorCuadrante2=0;
     AcumuladorCuadrante3=0;
     AcumuladorCuadrante4=0;
+    AcumuladorEje=0;
+
 
     printf("Bienvenido al juego\n");
     printf("Presione enter para continuar\n");
@@ -29,6 +30,7 @@ int main(){
     system("cls");
 
     for(int i=0;i<CantidadDeParticipantes;i++){
+        fflush(stdin);
         printf("Ingrese el nombre del participante[%d]\n",i+1);
         scanf("%s",Participantes[i]);
         system("cls");
@@ -46,46 +48,42 @@ int main(){
 
         for(int i=0;i<CantidadDeDisparos;i++){
             disparosX=((rand()%21)-10);
-            //printf("\n%d",disparosX);
+            printf("\n%d",disparosX);
             disparosY=((rand()%21)-10);
-            //printf("\t %d\n",disparosY);
-            //printf("------------\n");
-            if(disparosX>=0 && disparosY>0){
-                AcumuladorCuadrante1=AcumuladorCuadrante1+1;
+            printf("\t %d\n",disparosY);
+            printf("------------\n");
+            if(disparosX>0 && disparosY>0){
+                AcumuladorCuadrante1++;
             }
-            if(disparosX<0 && disparosY>=0){
-                AcumuladorCuadrante2=AcumuladorCuadrante2+1;
+            if(disparosX<0 && disparosY>0){
+                AcumuladorCuadrante2++;
             }
             if(disparosX<0 && disparosY<0){
-                AcumuladorCuadrante3=AcumuladorCuadrante3+1;
+                AcumuladorCuadrante3++;
             }
-            if(disparosX>=0 && disparosY<0){
-                AcumuladorCuadrante4=AcumuladorCuadrante4+1;
+            if(disparosX>0 && disparosY<0){
+                AcumuladorCuadrante4++;
             }
             if(disparosX==0 && disparosY==0){
-                AcumuladorBlanco=AcumuladorBlanco+1;
-        }
+                AcumuladorBlanco++;
+            }
+            if (disparosX==0 || disparosY==0){
+                AcumuladorEje++;
+            }
+
     }
+        printf("Presione enter para continuar\n");
+        getch();
+        system("cls");
+    }
+
     printf("Disparos en el cuadrante 1: %d\n",AcumuladorCuadrante1);
     printf("Disparos en el cuadrante 2: %d\n",AcumuladorCuadrante2);
     printf("Disparos en el cuadrante 3: %d\n",AcumuladorCuadrante3);
     printf("Disparos en el cuadrante 4: %d\n",AcumuladorCuadrante4);
+    printf("Disparos sobre el eje: %d\n",AcumuladorEje);
     printf("Disparos en el blanco: %d\n",AcumuladorBlanco);
 
-    if(AcumuladorBlanco){
-        Puntaje[j]=AcumuladorBlanco;
-        printf("\nPuntaje: %d\n",Puntaje[j]);
-    }
-    printf("\nPresione enter para continuar\n");
-    getch();
-    system("cls");
-    }
-
-    printf("Puntaje de los participantes\n");
-    for(int i=0;i<CantidadDeParticipantes;i++){
-        printf("%s:\n",Participantes[i]);
-        printf("\t%d\n",Puntaje[i]);
-    }
     getch();
     return 0;
 }
